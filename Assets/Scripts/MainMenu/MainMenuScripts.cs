df6627eb62b95cd00d527affbb4ef782f4195391
MatthewSiepka <matthew.siepka@gmail.com>
2024-11-30 18:54:16 +0100

    Added Credits Scene, Added MainMenu and Credits controls

---
+++ Assets/Scripts/MainMenu/MainMenuScripts.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScripts : MonoBehaviour
+{
+    public static void PlayGame() {
+        
+    }
+
+    public static void OpenCredits() {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public static void OpenHowToPlay() {
+        
+    }
+
+    public static void OpenSettings() {
+        
+    }
+}
