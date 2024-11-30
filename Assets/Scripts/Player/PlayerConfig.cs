7878eb50f20ec2a287ceadd44c610ae9b7b96080
MateuszCzerniawski <128147019+MateuszCzerniawski@users.noreply.github.com>
2024-11-30 18:06:18 +0100

    merge conflict solved

--- Assets/Scripts/Player/PlayerConfig.cs
+++ Assets/Scripts/Player/PlayerConfig.cs
@@ -6,9 +6,6 @@ namespace Player
     public class PlayerConfig : ScriptableObject
     {
         //Player movement
-        public float speed = 10f;
-        public float acceleration = 10f;
-        public float deceleration = 10f;
         public float knockBackFreezeTime = 0.2f;
         public float climbSpeed = 0.5f;
         public float speed = 4f;
